18d7b1cda11ca03ef6b3606ab095a54c15c30a2d
fundacion <fundacion@PTALENTI092>
2026-02-21 13:39:31 +0100

    Hecho el código

--- src/juego/Utils.java
+++ src/juego/Utils.java
@@ -21,6 +21,20 @@ public class Utils {
     /*
      * PATRI
      */
+    public static boolean esPrimo(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
     public static long factorial(int n) {
         // TODO: Implementar con bucle
         return 0;
