39b3aa0325e704838d2e116274df3bff5728a426
am7dam1 <am7dam1@PC07DAM1.informatica.iesjc>
2026-02-23 09:57:55 +0100

    productorio hecho

--- src/juego/Utils.java
+++ src/juego/Utils.java
@@ -9,14 +9,13 @@ public class Utils {
 		return suma;
 		}
 
-    /*
-     * AYOUB
-     */
-    public static long productorio(int a, int b) {
-        // TODO: Implementar con bucle
-        return 0;
-    }
-
+	public static long productorio(int a, int b) {
+		long producto = 1;
+		for (int i = a; i <= b; i++) {
+		producto *= i;
+		}
+		return producto;
+		}
     /*
      * PATRI
      */
